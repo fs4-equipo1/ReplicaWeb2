36c01b809a372f0956e0b9ab486a8fa6839c9496
LuisTM98 <luistortosam@gmail.com>
2023-11-20 17:29:01 +0100

    funcionalidad creada, falta estilizado

---
+++ funcionalidades botones/script.js
@@ -0,0 +1,13 @@
+
+
+
+var boton = document.getElementById('settingsButton')
+var modal = document.getElementById('elModal')
+
+function openModal() {
+    modal.style.display = 'flex'
+}
+
+function closeModal(){
+    modal.style.display = 'none'
+}
